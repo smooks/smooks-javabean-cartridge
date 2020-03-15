f46f9b7c130240d4a3bfda7b6e2889e938d27e32
claude.mamo <claude.mamo@gmail.com>
2020-03-15 12:43:16 +0100

    Deploying parent module

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-mvn clean deploy -pl cartridge --settings .mvn/settings.xml -Dgpg.skip=false -DskipTests=true -Dmaven.javadoc.skip=true -B
+mvn clean deploy -pl !perfcomp --settings .mvn/settings.xml -Dgpg.skip=false -DskipTests=true -Dmaven.javadoc.skip=true -B
